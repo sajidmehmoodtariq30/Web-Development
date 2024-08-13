db55f75b2a94b67dac7bef98f544fba22c810fd4
Sajid M Tariq <sajidmehmoodtariq5@gmail.com>
2024-08-13 21:42:25 +0500

    about console

---
+++ Front-End/03_JS/01_Basics/01_console.js
@@ -0,0 +1,29 @@
+/*
+    JS is a scripting language. Can be used for both server side and client side.
+    Now I have saved the file as 01.js where 01 is the name of file and js is its extension. Entension describes the type of file which in this case is JavaScript file.
+    If we define the file extension then it is understood that text written in that file is not random and each word has a specific meaning. Like if we use .c extension then compiler will check it according to the rules of c.
+    Similalrly if we use .js extension compiler or in this case runtime environment will  check it according to the rules of JavaScript.    
+*/
+//is an object that provides access to the browser's debugging console. It allows to log messages, errors, warnings, and other useful information for debugging purposes. The most commonly used method of the console object is 
+// console.log(), which outputs messages to the console.
+console.log("Sajid Mehmood Tariq");
+// console.error() Logs an error message to the console.
+console.error("This is an error");
+// console.warn() Logs a warning message to the console.
+console.warn("This is a warning");
+// console.info() Logs an informational message to the console.
+console.info("This is an info");
+// console.table() Logs an array or object to the console, formatted as a table.
+console.table({name: "Sajid Mehmood Tariq", age: 20});
+
+console.clear(); // Clears all previous messages from the console.
+console.count(); // Counts the number of times console.count() has been called.
+
+console.countReset(); // Resets the count to 0.
+
+// console.assert()  Asserts a condition, and logs a message to the console if it's false
+console.assert(1 === 2, "1 is not equal to 2");
+console.assert(1 === 1, "1 is not equal to 1");
+console.assert(1 === 2, "1 is not equal to 2");
+
+
