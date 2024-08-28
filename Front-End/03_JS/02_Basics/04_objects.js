9ea139b660480b6f6d2131f370b578c0eccadbcd
Sajid M Tariq <sajidmehmoodtariq5@gmail.com>
2024-08-28 05:53:17 +0500

    minor changes

--- Front-End/03_JS/02_Basics/04_objects.js
+++ Front-End/03_JS/02_Basics/04_objects.js
@@ -11,6 +11,9 @@ usrProfile.fullname = {
 const obj1 = {1:'a',2:'b'}
 const obj2 = {3:'c',4:'d'}
 
-const obj3 = Object.assign(obj1,obj2);
+// const obj3 = Object.assign({},obj1,obj2);  // (target , sources)
+
+
+const obj3 = {...obj1, ... obj2}
 console.log(obj3);
 
